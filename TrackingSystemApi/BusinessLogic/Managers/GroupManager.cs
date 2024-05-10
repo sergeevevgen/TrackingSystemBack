e4a05faac04c74078dbc807a464594dfa975edf1
sergeevevgen <alexevgenii@mail.ru>
2024-05-10 20:57:09 +0400

    +

--- TrackingSystemApi/BusinessLogic/Managers/GroupManager.cs
+++ TrackingSystemApi/BusinessLogic/Managers/GroupManager.cs
@@ -1,4 +1,5 @@
 ﻿using TrackingSystem.Api.Shared.Dto.Group;
+using TrackingSystem.Api.Shared.Dto.Lesson;
 using TrackingSystem.Api.Shared.IManagers.DbManagers;
 using TrackingSystem.Api.Shared.IManagers.LogicManagers;
 using TrackingSystem.Api.Shared.SharedModels;
@@ -8,29 +9,64 @@ namespace TrackingSystem.Api.BusinessLogic.Managers
     public class GroupManager : IGroupManager
     {
         private readonly ILogger _logger;
-        private readonly IGroupDbManager _manager;
+        private readonly IGroupDbManager _storage;
 
         public GroupManager(
             ILogger logger,
             IGroupDbManager manager)
         {
             _logger = logger;
-            _manager = manager;
+            _storage = manager;
         }
 
-        public Task<GroupResponseDto> CreateOrUpdate(GroupDto model, CancellationToken cancellationToken)
+        public async Task<GroupResponseDto> CreateOrUpdate(GroupDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var element = await _storage.GetElement(new GroupDto
+            {
+                Name = model.Name,
+            }, cancellationToken);
+
+            if (element != null && element.Id != model.Id)
+            {
+                _logger.Error("Уже есть группа с такими названием");
+                throw new Exception("Уже есть группа с таким названием");
+            }
+
+            if (model.Id.HasValue)
+            {
+                element = await _storage.Update(model, cancellationToken);
+                _logger.Info($"Группа с идентификатором {model.Id} обновлена");
+            }
+            else
+            {
+                element = await _storage.Insert(model, cancellationToken);
+                _logger.Info($"Создана новая группа");
+            }
+
+            return element;
         }
 
-        public Task<bool> Delete(GroupDto model, CancellationToken cancellationToken)
+        public async Task<bool> Delete(GroupDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _ = await _storage.GetElement(new GroupDto
+            {
+                Id = model.Id,
+            }, cancellationToken) ?? throw new Exception($"Элемент с идентификатором {model.Id} не найден");
+
+            await _storage.Delete(model, cancellationToken);
+
+            return true;
         }
 
-        public Task<ResponseModel<GroupResponseDto>> Read(GroupDto model, CancellationToken cancellationToken)
+        public async Task<ResponseModel<GroupResponseDto>> Read(GroupDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (model.Id.HasValue)
+            {
+                var data = await _storage.GetElement(model, cancellationToken);
+                return new ResponseModel<GroupResponseDto> { Data = data };
+            }
+
+            return new ResponseModel<GroupResponseDto> { ErrorMessage = $"Такая группа не найдена {model.Name}" };
         }
 
         public Task<ResponseModel<List<GroupResponseDto>>> ReadAll(List<GroupDto> model, CancellationToken cancellationToken)
--- TrackingSystemApi/BusinessLogic/Managers/PlaceManager.cs
+++ TrackingSystemApi/BusinessLogic/Managers/PlaceManager.cs
@@ -1,4 +1,5 @@
-﻿using TrackingSystem.Api.Shared.Dto.Place;
+﻿using TrackingSystem.Api.Shared.Dto.Lesson;
+using TrackingSystem.Api.Shared.Dto.Place;
 using TrackingSystem.Api.Shared.IManagers.DbManagers;
 using TrackingSystem.Api.Shared.IManagers.LogicManagers;
 using TrackingSystem.Api.Shared.SharedModels;
@@ -8,29 +9,64 @@ namespace TrackingSystem.Api.BusinessLogic.Managers
     public class PlaceManager : IPlaceManager
     {
         private readonly ILogger _logger;
-        private readonly IPlaceDbManager _manager;
+        private readonly IPlaceDbManager _storage;
 
         public PlaceManager(
             ILogger logger,
             IPlaceDbManager manager)
         {
             _logger = logger;
-            _manager = manager;
+            _storage = manager;
         }
 
-        public Task<PlaceResponseDto> CreateOrUpdate(PlaceDto model, CancellationToken cancellationToken)
+        public async Task<PlaceResponseDto> CreateOrUpdate(PlaceDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var element = await _storage.GetElement(new PlaceDto
+            {
+                Name = model.Name,
+            }, cancellationToken);
+
+            if (element != null && element.Id != model.Id)
+            {
+                _logger.Error("Уже есть помещение с таким названием");
+                throw new Exception("Уже есть помещение с таким названием");
+            }
+
+            if (model.Id.HasValue)
+            {
+                element = await _storage.Update(model, cancellationToken);
+                _logger.Info($"Помещение с идентификатором {model.Id} обновлен");
+            }
+            else
+            {
+                element = await _storage.Insert(model, cancellationToken);
+                _logger.Info($"Создано новое помещение");
+            }
+
+            return element;
         }
 
-        public Task<bool> Delete(PlaceDto model, CancellationToken cancellationToken)
+        public async Task<bool> Delete(PlaceDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            _ = await _storage.GetElement(new PlaceDto
+            {
+                Id = model.Id,
+            }, cancellationToken) ?? throw new Exception($"Элемент с идентификатором {model.Id} не найден");
+
+            await _storage.Delete(model, cancellationToken);
+
+            return true;
         }
 
-        public Task<ResponseModel<PlaceResponseDto>> Read(PlaceDto model, CancellationToken cancellationToken)
+        public async Task<ResponseModel<PlaceResponseDto>> Read(PlaceDto model, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            if (model.Id.HasValue)
+            {
+                var data = await _storage.GetElement(model, cancellationToken);
+                return new ResponseModel<PlaceResponseDto> { Data = data };
+            }
+
+            return new ResponseModel<PlaceResponseDto> { ErrorMessage = $"Такое помещение не найдено {model.Name}" };
         }
 
         public Task<ResponseModel<PlaceResponseDto>> ReadAll(List<PlaceDto> model, CancellationToken cancellationToken)
